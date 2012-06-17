4d10b51194f4e21de7a5a143f8052ca06911dc25
Pedro Reys <pedroreys@gmail.com>
2012-06-17 17:59:23 -0500

    initial commit

---
+++ WinFormPreviewHostSpike/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormPreviewHostSpike
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            var result = dialog.ShowDialog();
+            
+            if(result == DialogResult.OK)
+            {
+                previewHandlerHost.Open(dialog.FileName);
+            }
+        }
+    }
+}
